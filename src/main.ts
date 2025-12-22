4c9d4fd9f0ecd95c64ec36228411f8ae48c62813
Daniel Brito <damasceno1871@gmail.com>
2025-12-22 15:51:32 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
